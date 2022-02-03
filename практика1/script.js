084eaee28f86e05828a6a34d3e9724640800b518
TsekhanDmytro <tsekhandmytro@gmail.com>
2022-02-03 17:05:11 +0200

    лекція2, практика1

--- практика1/script.js
+++ практика1/script.js
@@ -70,7 +70,7 @@
 // результат помістіть у змінну v.
 // let heightC =10
 // let dC = 4
-// let v = let v = PI * Math.pow(dC / 2, 2) * heightC
+// let v = Math.PI * Math.pow(dC / 2, 2) * heightC
 // console.log(v)
 
 
