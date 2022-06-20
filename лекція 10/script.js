0637e98fc69110ecdedf7d3ae554eb3c89c6aa00
TsekhanDmytro <tsekhandmytro@gmail.com>
2022-06-20 20:23:24 +0300

    Домашнє завдання 10 не завершене

--- лекція 10/script.js
+++ лекція 10/script.js
@@ -1,10 +1,52 @@
 // - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
 //
+// let buttonElement = document.createElement('button')
+// let divElement = document.createElement('div')
+// buttonElement.innerText= 'click my'
 //
+// divElement.id ='text';
+// divElement.style.backgroundColor ='blue';
+// divElement.style.height = '200px';
+// divElement.style.width = '200px';
+// document.body.appendChild(buttonElement);
+// document.body.appendChild(divElement)
+// buttonElement.onclick = ()=>{
+//     console.log('click')
+// let text = document.getElementById('text')
+//  text.style.display= 'none'
+// }
+
+
+
 // - створити інпут який приймає вік людини та кнопку яка підтверджує дію.
 // При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
 //
+// let form = document.createElement('form')
+// let inputElement = document.createElement('input')
+// let buttonElement= document.createElement('button')
+// -------------------------------------------------не завершенне завдання---------------------
+// inputElement.name= 'age'
+// buttonElement.innerText= 'click'
+// inputElement.type= 'number'
 //
+// document.body.appendChild(form)
+// form.appendChild(inputElement)
+// form.appendChild(buttonElement)
+// form.onsubmit =function (e) {
+//     e.preventDefault()
+//  console.log(this.age.value)
+//     if (e>18){
+// console.log('aaa')
+//     }
+//     else console.log('sss')
+// }
+
+
+
+
+
+
+
 // - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та виводиться на консоль інформація з цих 2х форм.
 //     Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
 // Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
@@ -13,3 +55,6 @@
 // - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
+
+
+
